ef4ebb22a55415f91bbce2172004d75ab60b0ca7
zetaminer <juliacook@gmail.com>
2018-04-23 16:00:24 -0500

    made balloon move
    made balloon move

--- balloon.java
+++ balloon.java
@@ -8,23 +8,27 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class balloon extends Actor
 {
+    
     GreenfootImage image = new GreenfootImage("balloon1.png");
     double percentChange = -0.9;
     double width = (double)(image.getWidth());
     double height =(double)(image.getHeight());
+    int degrees = getRandomNumber(0,359);
      /**
      * Act - do whatever the ballon wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act() 
     {
-       while (percentChange < 10)  // change to isAtEdge == False
+       while (isAtEdge() == false)  // change to isAtEdge == False
        {
         //GreenfootImage image = new GreenfootImage("balloon1.png");
         scaling(percentChange);
         percentChange += .1;
+        moveBalloon();
         Greenfoot.delay(5);
        }
+       getWorld().removeObject(this);
     }     
     public void scaling(double percentChange)
     {
@@ -32,8 +36,15 @@ public class balloon extends Actor
         image.scale((int)(width + percentChange*width), (int)(height + percentChange*height)); 
         setImage(image);
     }
-    public void moving()
+    public void moveBalloon()
     {
-        
+        setRotation(degrees);
+        turn(getRandomNumber(-20,20));
+        move(10);
+    }
+    public int getRandomNumber(int start,int end)
+    {
+       int normal = Greenfoot.getRandomNumber(end-start+1);
+       return normal+start;
     }
 }
